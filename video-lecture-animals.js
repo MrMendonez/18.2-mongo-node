22a4e87e1b06fa4856c8f3a91132dbf7ccd8e6d4
MrMendonez <dmendonez@gmail.com>
2016-03-11 19:23:05 -0500

    got animals route to display all animals in json format

--- video-lecture-animals.js
+++ video-lecture-animals.js
@@ -1,9 +1,25 @@
 var express = require("express");
 var app = express();
 var PORT = 8080;
+var mongojs = require('mongojs')
+var db = mongojs("zoo", ["animals"])
+db.on('error', function(err) {
+  console.log("database error: " + err);
+});
 
 app.get('/', function(req, res) {
   res.send("Hello world!");
+})
+
+app.get('/animals', function(req, res) {
+  db.animals.find({}, function(err, documents) {
+    if(err) {
+      console.log(err);
+    } 
+    else {
+      res.json(documents);
+    }
+  })
 })
 
 app.listen(PORT, function() {
