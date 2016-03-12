009d42eb7793f40d1c70b898af6f978459aacf1b
MrMendonez <dmendonez@gmail.com>
2016-03-11 20:26:41 -0500

    got buttons to console.log data

--- public/app.js
+++ public/app.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
   console.log("This worked!");
 
   $("#weightsort").on("click", function() {
+    // get and log to the console data from the /weight endpoint
+    $.getJSON("/weight", function(data) {
+      console.log(data);
+    })
     console.log("Weight button clicked!");
   });
 
   $("#namesort").on("click", function() {
+    $.getJSON("/name", function(data) {
+      console.log(data);
+    })
     console.log("Name button clicked!");
   });
 })
